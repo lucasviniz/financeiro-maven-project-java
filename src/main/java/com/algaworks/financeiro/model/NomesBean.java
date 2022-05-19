6d03df4ddda8d51d03ce83a36e32a70554f4dfdf
Lucas Vinicius <62735785+lucasviniz@users.noreply.github.com>
2022-05-19 14:03:32 -0300

    Add files via upload

---
+++ src/main/java/com/algaworks/financeiro/model/NomesBean.java
@@ -0,0 +1,35 @@
+package com.algaworks.financeiro.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@SessionScoped
+public class NomesBean {
+	private String nome;
+	private List<String> nomes = new ArrayList<> ();
+	
+	
+	public void adicionar() {
+		this.nomes.add(nome);
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public List<String> getNomes() {
+		return nomes;
+	}
+	
+	
+}
